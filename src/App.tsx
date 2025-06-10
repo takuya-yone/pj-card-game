6d924c167ed34e9ef1d2bed460baa17ca52c56cc
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2025-06-10 20:13:59 +0900

    feat: implement Card and Button components, update App layout with new UI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import { useGameField } from '@/hooks/useGameField'
 import '@/App.css'
+import { Button } from "@/components/ui/button"
+import { CardView } from '@/components/cardView'
 
 function App() {
 	const { deck, shuffle, player, draw, multiDraw, reset } = useGameField()
 
 	return (
 		<>
-			<p>{deck.cards.length}</p>
-			<p>{JSON.stringify(deck.cards)}</p>
-			<button onClick={() => draw(deck)}>draw</button>
-			<button onClick={() => multiDraw(deck, 3)}>multiDraw</button>
-			<button onClick={() => shuffle()}>shuffle</button>
-			<button onClick={() => reset()}>reset</button>
-			<p>{player.cards.length}</p>
-			<p>{JSON.stringify(player.cards)}</p>
+			<div className='flex flex-col items-center justify-center h-screen'>
+				<div className='bg-green-100 p-2 rounded-lg mb-4'>
+					<p>{deck.cards.length}</p>
+					<CardView cards={deck.cards} />
+				</div>
+				<div className="inline-flex">
+					<div className='p-1'><Button className='bg-amber-200'>asdfja;flj</Button></div>
+					<div className='p-1'><Button className='bg-amber-200' onClick={() => draw(deck)}>draw</Button></div>
+					<div className='p-1'><Button className='bg-amber-200' onClick={() => multiDraw(deck, 3)}>multiDraw</Button></div>
+					<div className='p-1'><Button className='bg-amber-200' onClick={() => shuffle()}>shuffle</Button></div>
+					<div className='p-1'><Button className='bg-amber-200' onClick={() => reset()}>reset</Button></div>
+				</div>
+				<div className='bg-blue-100 p-2 rounded-lg mt-4'>
+					<p>{player.cards.length}</p>
+					<CardView cards={player.cards} />
+				</div>
+			</div>
 		</>
 	)
 }
---
+++ src/components/cardView.tsx
@@ -0,0 +1,26 @@
+import {
+    Card as CardComp,
+    CardContent,
+} from "@/components/ui/card"
+import { Card } from '@/models/Card'
+
+
+function CardView(props: { cards: Card[] }) {
+
+    return (
+        <div className="grid grid-cols-4 gap-4">
+            {
+                props.cards.map((card, index) => (
+                    <CardComp key={index}>
+                        <CardContent>
+                            <p className="truncate">{card.id}</p>
+                        </CardContent>
+                    </CardComp>
+                ))
+            }
+        </div>
+    )
+
+}
+
+export { CardView }
