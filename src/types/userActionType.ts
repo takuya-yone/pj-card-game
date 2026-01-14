3c891cfcd5cba6e5c6fc8da6aa502c8131ffce13
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2026-01-14 21:05:22 +0900

    feat: define UserActionType and _EventType for game actions and events

--- src/types/userActionType.ts
+++ src/types/userActionType.ts
@@ -1,1 +1,24 @@
+import { Deck } from '@/models/Deck'
+import { Player } from '@/models/Player'
 export type UserActionType = 'draw' | 'multiDraw' | 'shuffle' | 'reset'
+
+export type _EventType =
+  | {
+      eventName: 'DRAW'
+      deck: Deck
+      player: Player
+    }
+  | {
+      eventName: 'MULTI_DRAW'
+      deck: Deck
+      player: Player
+    }
+  | {
+      eventName: 'SHUFFLE'
+      deck: Deck
+    }
+  | {
+      eventName: 'RESET'
+      deck: Deck
+      player: Player
+    }
